f1a133aca916ae9d5eea63ee8fe59a6fb25d98a6
asiftariq1 <aasifakakll25@gmail.com>
2024-02-26 22:00:11 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,84 @@
+// Chapter No:4
+// Q no:1
+
+// var useername =
+// var useername =
+// var useername =
+
+// Q no:2
+// legal variable :.
+
+// 1:My_name
+// 2:$my_name
+// 3:_my_name
+// 4:My_name_first
+// 5:myFirstname
+
+//  illegal variable:.
+// 1:1my_name
+// 2:@my_name3:
+// 3:++my_name
+// 4:#myname
+// 5:%myname
+
+// Q no:3
+
+// "Rules for naming js variable"
+// variable name only contains ,numbers $ and _. which are  My_name, _my_name,myFirstname,My_name_first.
+// Variable must begin with aletter, $ or _. For example ; $name,_name or name
+// Variable names are case sensitive
+// Variable names should notbe js keywords.
+
+
+// Chapter No:5
+// Q no:1
+
+// var suma = 5 
+// var sumb = 4
+// var sumc = suma + sumb
+// document.write("sum of"sums "and"sumb"in"sumc)
+
+// Q no:2
+
+// var suma = 5 
+// var sumb = 4
+// var sumc = suma - sumb
+// document.write("sum of"sums "and"sumb"in"sumc)
+
+// Q no:3
+
+// var num = 5= num++ -num--
+
+
+// Q no:4
+
+// var ticketprice = 600
+// document.write(ticketprice*5);
+
+// Q no:5
+
+// var num = 7
+// document.write(num + "x 1 =" + num * 1 +"<br>"+
+// num + "x 2 =" + num * 2 +"<br>"+
+// num + "x 3 =" + num * 3 +"<br>"+
+// num + "x 4 =" + num * 4 +"<br>"+
+// num + "x 5 =" + num * 5 +"<br>"+
+// num + "x 6 =" + num * 6 +"<br>"+
+// num + "x 7 =" + num * 7 +"<br>"+
+// num + "x 8 =" + num * 8 +"<br>"+
+// num + "x 9 =" + num * 9 +"<br>"+
+// num + "x 10 =" + num * 10 +"<br>"+)
+
+// Q no:6
+
+
+// Q no:7
+// Q no:8
+// Q no:9
+// Q no:10
+// Q no:11
+// Q no:12
+// Q no:13
+
+
+
